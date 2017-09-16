67337bc4b9ece983fb710cdfa82fec6308c7e069
Andrew Che <andrew@codeninja55.me>
2017-09-16 21:25:53 +1000

    Changed all button impl to have an icon

--- com.cn55.marvel/src/application/view/CustomComponents/ToolbarButton.java
+++ com.cn55.marvel/src/application/view/CustomComponents/ToolbarButton.java
@@ -1,15 +1,16 @@
 package application.view.CustomComponents;
 
-import javax.swing.JButton;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class ToolbarButton extends JButton {
 
-    public ToolbarButton(String text) {
-        super(text);
+    public ToolbarButton(String text, ImageIcon icon) {
+        super(" "+text);
 
+        setIcon(icon);
         setFont(Style.toolbarButtonFont());
         setBackground(Style.red500());
         setForeground(Style.btnTextColor());
@@ -19,7 +20,6 @@ public class ToolbarButton extends JButton {
         setVisible(true);
 
         this.addMouseListener(new MouseAdapter() {
-            @Override
             public void mouseEntered(MouseEvent e) {
                 super.mouseEntered(e);
                 if (isEnabled()) setBackground(Style.blueGrey500());
