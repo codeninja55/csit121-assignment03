8391fe6e81a84b7afd30cf1df26b4319f5c46786
Andrew Che <andrew@codeninja55.me>
2017-09-20 14:19:41 +1000

    Added a rule for Categories existing

---
+++ com.cn55.marvel/src/application/controller/validator/CategoryExistsRule.java
@@ -0,0 +1,7 @@
+package application.controller.validator;
+import application.model.Shop;
+public class CategoryExistsRule implements ExistsRule {
+    public boolean existsValidating(FormValidData validData) {
+        return Shop.getShopInstance().getDataStore().getCategory(validData.getCategoryID()) != null;
+    }
+}
