42ebfe255a16ea88f4c492a1e415a187d8fed11d
Andrew Che <andrew@codeninja55.me>
2017-09-20 14:23:41 +1000

    Changed the way category id's are valid

--- com.cn55.marvel/src/application/controller/validator/CategoryIDRule.java
+++ com.cn55.marvel/src/application/controller/validator/CategoryIDRule.java
@@ -1,12 +1,10 @@
 package application.controller.validator;
-
 import java.util.regex.Pattern;
-
 public class CategoryIDRule implements FormRule {
     private static final String ID_PATTERN = "\\d*";
     private static final Pattern regex = Pattern.compile(ID_PATTERN);
 
     public boolean validate(FormValidData validData) {
-        return regex.matcher(validData.getCategoryID()).matches();
+        return regex.matcher(validData.getCategoryIDStr()).matches();
     }
 }
--- com.cn55.marvel/src/application/controller/validator/FormValidData.java
+++ com.cn55.marvel/src/application/controller/validator/FormValidData.java
@@ -4,6 +4,7 @@ package application.controller.validator;
 public class FormValidData {
     private String cardID;
     private String categoryIDStr;
+    private int categoryID;
     private String catValueStr;
     private Double catValue;
     private String email;
@@ -18,44 +19,36 @@ public class FormValidData {
     }
 
     /*============================== MUTATORS ==============================*/
-    public void setCardID(String cardID) {
-        this.cardID = cardID.toUpperCase();
-    }
-
-    public void setCategoryID(String categoryIDStr) {
+    public void setCardID(String cardID) { this.cardID = cardID.toUpperCase(); }
+    public void setCategoryIDStr(String categoryIDStr) {
         this.categoryIDStr = categoryIDStr;
     }
-
     public void setCatValueStr(String catValueStr) {
         this.catValueStr = catValueStr;
     }
-
     public void setCatValue(Double catValue) {
         this.catValue = catValue;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
+    public void setCategoryID(int categoryID) { this.categoryID = categoryID; }
 
     /*============================== ACCESSORS ==============================*/
     public String getCardID() {
         return cardID;
     }
-
-    public String getCategoryID() {
-        return categoryIDStr;
-    }
-
     public String getCatValueStr() {
         return catValueStr;
     }
-
     public Double getCatValue() {
         return catValue;
     }
-
     public String getEmail() {
         return email;
+    }
+    public String getCategoryIDStr() { return categoryIDStr; }
+    public int getCategoryID() {
+        return categoryID;
     }
 }
