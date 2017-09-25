58d18115a456855369cb0e5ec8be7d0dfb442a0b
Andrew Che <andrew@codeninja55.me>
2017-09-26 01:32:23 +1000

    Added a constructor to change colour of font

--- com.cn55.marvel/src/application/view/customComponents/FormLabel.java
+++ com.cn55.marvel/src/application/view/customComponents/FormLabel.java
@@ -1,12 +1,18 @@
 package application.view.customComponents;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class FormLabel extends JLabel {
-
     public FormLabel(String text) {
         super(text);
         setFont(Style.labelFont());
         setVisible(false);
+    }
+
+    public FormLabel (String text, Color foreground) {
+        this(text);
+        setForeground(foreground);
+        setVisible(true);
     }
 }
