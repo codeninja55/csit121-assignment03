e175fc491baa7caef1a4e73a82984aecd39a3526
Andrew Che <andrew@codeninja55.me>
2017-09-27 12:52:57 +1000

    Import cleanup

--- com.cn55.marvel/src/application/view/MainFrame.java
+++ com.cn55.marvel/src/application/view/MainFrame.java
@@ -6,12 +6,10 @@ import styles.IconFactory;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.net.URL;
-import java.util.Arrays;
 
 public class MainFrame extends JFrame {
     private final JTabbedPane tabPane;
