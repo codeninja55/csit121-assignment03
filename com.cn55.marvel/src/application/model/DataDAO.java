90655a97a1822fe96a7c3a11286ce4f2f3102f13
Andrew Che <andrew@codeninja55.me>
2017-09-30 01:26:46 +1000

    Completed impl of authentication for username and password
    Using SHA-256 hasing with MessageDigest. Passwords are added with a further deafult salt. Two generate hash methods turn strings into hashes for storing. Two further methods allow for saving those generated hashes into the Preferences

--- com.cn55.marvel/src/application/model/DataDAO.java
+++ com.cn55.marvel/src/application/model/DataDAO.java
@@ -226,4 +226,67 @@ public class DataDAO implements DataObservable, CardsDAO, PurchaseDAO, CategoryD
     // NOTE: This returns a shallow copy only
     public LocalDateTime getFirstPurchaseDate(DataObserver who) { return firstPurchaseDate; }
     public LocalDateTime getLastPurchaseDate(DataObserver who) { return lastPurchaseDate; }
+
+    /*============================== AUTHENTICATION ==============================*/
+    public void signup(String username, char[] password) {
+        String saltedPassword = SALT + new String(password);
+
+        try {
+            programSettings.put("username", generatedHashUsername(username));
+            programSettings.put("password", generateHashPassword(saltedPassword));
+            programSettings.flush();
+        } catch (BackingStoreException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean login(String username, char[] password) {
+        String hashedUsername = generatedHashUsername(username);
+        String saltedPassword = SALT + new String(password);
+        String hashedPassword = generateHashPassword(saltedPassword);
+
+        String storedUsername = programSettings.get("username", "");
+        String storedPassword = programSettings.get("password", "");
+
+        return storedUsername.equals(hashedUsername) && storedPassword.equals(hashedPassword);
+    }
+
+    private String generateHashPassword(String saltedPassword) {
+        StringBuilder hash = new StringBuilder();
+        String generatedPassword = null;
+
+        try {
+            MessageDigest hasher = MessageDigest.getInstance("SHA-256");
+            byte[] hashedBytes = hasher.digest(saltedPassword.getBytes());
+
+            for (byte hashedByte : hashedBytes) {
+                hash.append(Integer.toString((hashedByte & 0xff) + 0x100, 16).substring(1));
+            }
+
+            generatedPassword = hash.toString();
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("NoSuchAlgorithmException: " + e.getCause());
+        }
+        return generatedPassword;
+    }
+
+    private String generatedHashUsername(String username) {
+        StringBuilder hash = new StringBuilder();
+        String generatedUsername = null;
+
+        try {
+            MessageDigest hasher = MessageDigest.getInstance("SHA-1");
+            byte[] hashedBytes = hasher.digest(username.getBytes());
+            char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                    'a', 'b', 'c', 'd', 'e', 'f' };
+
+            for (byte b : hashedBytes) {
+                hash.append(digits[(b & 0xf0) >> 4]);
+                hash.append(digits[b & 0x0f]);
+            }
+            generatedUsername = hash.toString();
+        } catch (NoSuchAlgorithmException e) { System.out.println("NoSuchAlgorithmException: " + e.getCause()); }
+
+        return generatedUsername;
+    }
 }
