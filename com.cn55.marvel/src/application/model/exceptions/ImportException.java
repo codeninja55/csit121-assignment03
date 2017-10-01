48b81bf6beaf08d7e65a786aa142b95bc01bc101
Andrew Che <andrew@codeninja55.me>
2017-10-02 04:35:57 +1100

    Changed name and also used another constructor with throwable cause

---
+++ com.cn55.marvel/src/application/model/exceptions/ImportException.java
@@ -0,0 +1,7 @@
+package application.model.exceptions;
+
+public class ImportException extends Exception {
+    public ImportException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
