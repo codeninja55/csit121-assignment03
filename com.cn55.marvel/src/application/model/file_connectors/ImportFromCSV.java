94226ed88bc637acd54764d93bf0895489a166bc
Andrew Che <andrew@codeninja55.me>
2017-10-02 04:37:22 +1100

    Name changed for ImportException

--- com.cn55.marvel/src/application/model/file_connectors/ImportFromCSV.java
+++ com.cn55.marvel/src/application/model/file_connectors/ImportFromCSV.java
@@ -1,12 +1,12 @@
 package application.model.file_connectors;
 
 import application.model.dao.DataStoreDAO;
-import application.model.exceptions.ImportEmptyException;
+import application.model.exceptions.ImportException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 
 /* USING A STRATEGY DESIGN PATTERN */
 public interface ImportFromCSV extends CSV {
-    void executeImport(DataStoreDAO dataStore, BufferedReader reader) throws ImportEmptyException, IOException;
+    void executeImport(DataStoreDAO dataStore, BufferedReader reader) throws ImportException, IOException;
 }
