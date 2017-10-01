f5d4eaf00cb54b82f8290092567d4066a2de5f12
Andrew Che <andrew@codeninja55.me>
2017-10-01 15:58:24 +1100

    Optimized import/exports with streams

--- com.cn55.marvel/src/application/model/category/CategoriesExport.java
+++ com.cn55.marvel/src/application/model/category/CategoriesExport.java
@@ -8,30 +8,19 @@ import java.io.IOException;
 import java.time.Instant;
 
 public class CategoriesExport implements ExportToCSV {
-    private static final char DEFAULT_SEPARATOR = ',';
-    private BufferedWriter output;
-
-    public void exportData(DataStoreDAO db, BufferedWriter writer) throws IOException {
-        this.output = writer;
-
-        for (Category c : db.getAllCategories().values()) {
-            output.append(Instant.now().toString()).append(DEFAULT_SEPARATOR)
-                    .append(Integer.toString(c.getId())).append(DEFAULT_SEPARATOR)
-                    .append(c.getName()).append(DEFAULT_SEPARATOR)
-                    .append(c.getDescription()).append(DEFAULT_SEPARATOR)
-                    .append(Double.toString(c.getAmount()));
-            output.newLine();
-        }
-
-        closeFile();
-    }
-
-    private void closeFile() {
-        try {
-            output.flush();
-            output.close();
-        } catch (IOException e) {
-            System.err.println("IOException: " + e.getMessage());
-        }
+    public void exportData(DataStoreDAO dataStore, BufferedWriter writer) throws IOException {
+        dataStore.getAllCategories().values().parallelStream().forEach(c -> {
+            try {
+                writer.append(Instant.now().toString()).append(DEFAULT_SEPARATOR)
+                        .append(Integer.toString(c.getId())).append(DEFAULT_SEPARATOR)
+                        .append(c.getName()).append(DEFAULT_SEPARATOR)
+                        .append(c.getDescription()).append(DEFAULT_SEPARATOR)
+                        .append(Double.toString(c.getAmount()));
+                writer.newLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        ExportToCSV.closeWriter(writer);
     }
 }
