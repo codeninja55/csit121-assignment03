e643063aee0ae4616528f17e8eb76954394dfa6d
Andrew Che <andrew@codeninja55.me>
2017-10-01 15:58:24 +1100

    Optimized import/exports with streams

--- com.cn55.marvel/src/application/model/card/CardsExport.java
+++ com.cn55.marvel/src/application/model/card/CardsExport.java
@@ -8,28 +8,21 @@ import java.io.IOException;
 import java.time.Instant;
 
 public class CardsExport implements ExportToCSV {
-    private static final char DEFAULT_SEPARATOR = ',';
-    private BufferedWriter output;
-
-    public void exportData(DataStoreDAO db, BufferedWriter writer) throws IOException {
-        this.output = writer;
-
-        for (Card card : db.getAllCards().values()) {
-            output.append(Instant.now().toString()).append(DEFAULT_SEPARATOR)
-                    .append(card.getID()).append(DEFAULT_SEPARATOR)
-                    .append(card.getCardType()).append(DEFAULT_SEPARATOR)
-                    .append((card instanceof AdvancedCard) ? ((AdvancedCard) card).getName() : "").append(DEFAULT_SEPARATOR)
-                    .append((card instanceof AdvancedCard) ? ((AdvancedCard) card).getEmail() : "").append(DEFAULT_SEPARATOR)
-                    .append(Double.toString(card.getPoints())).append(DEFAULT_SEPARATOR)
-                    .append((card instanceof AdvancedCard) ? Double.toString(((AdvancedCard) card).getBalance()) : "");
-            output.newLine();
-        }
-
-        closeFile();
-    }
-
-    private void closeFile() throws IOException {
-        output.flush();
-        output.close();
+    public void exportData(DataStoreDAO dataStore, BufferedWriter writer) throws IOException {
+        dataStore.getAllCards().values().parallelStream().forEach(card -> {
+            try {
+                writer.append(Instant.now().toString()).append(DEFAULT_SEPARATOR)
+                        .append(card.getID()).append(DEFAULT_SEPARATOR)
+                        .append(card.getCardType()).append(DEFAULT_SEPARATOR)
+                        .append((card instanceof AdvancedCard) ? ((AdvancedCard) card).getName() : "").append(DEFAULT_SEPARATOR)
+                        .append((card instanceof AdvancedCard) ? ((AdvancedCard) card).getEmail() : "").append(DEFAULT_SEPARATOR)
+                        .append(Double.toString(card.getPoints())).append(DEFAULT_SEPARATOR)
+                        .append((card instanceof AdvancedCard) ? Double.toString(((AdvancedCard) card).getBalance()) : "");
+                writer.newLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        ExportToCSV.closeWriter(writer);
     }
 }
--- com.cn55.marvel/src/application/model/category/CategoriesImport.java
+++ com.cn55.marvel/src/application/model/category/CategoriesImport.java
@@ -1,6 +1,7 @@
 package application.model.category;
 
 import application.model.Generator;
+import application.model.dao.CSV;
 import application.model.dao.DataStoreDAO;
 import application.model.dao.ImportFromCSV;
 
@@ -8,20 +9,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 public class CategoriesImport implements ImportFromCSV {
-    private static final String DEFAULT_SEPARATOR = ",";
-
     public void executeImport(DataStoreDAO dataStore, BufferedReader reader) throws IOException {
-        String line;
-
-        while((line = reader.readLine()) != null) {
-            String[] readLine = line.split(DEFAULT_SEPARATOR);
+        reader.lines().forEach(line -> {
+            String[] readLine = line.split(CSV.DEFAULT_SEPARATOR_STR);
             // readLine[0] = id | [1] = name | [2] = description | [3] = amount
             Category importedCategory = new Category(Integer.parseInt(readLine[1]), readLine[2],
                     readLine[3], Double.parseDouble(readLine[4]));
 
             Generator.updateCategoryIDCounter();
             dataStore.createCategory(importedCategory);
-        }
+        });
         reader.close();
     }
 }
