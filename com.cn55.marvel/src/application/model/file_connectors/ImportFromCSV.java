486b70b21b85a13256ce3cfadcd08a8f4e207bd1
Andrew Che <andrew@codeninja55.me>
2017-10-02 03:46:36 +1100

    Added throwing of ImportEmptyException

--- com.cn55.marvel/src/application/model/file_connectors/ImportFromCSV.java
+++ com.cn55.marvel/src/application/model/file_connectors/ImportFromCSV.java
@@ -1,11 +1,12 @@
 package application.model.file_connectors;
 
 import application.model.dao.DataStoreDAO;
+import application.model.exceptions.ImportEmptyException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 
 /* USING A STRATEGY DESIGN PATTERN */
 public interface ImportFromCSV extends CSV {
-    void executeImport(DataStoreDAO dataStore, BufferedReader reader) throws IOException;
+    void executeImport(DataStoreDAO dataStore, BufferedReader reader) throws ImportEmptyException, IOException;
 }
