9d98b3df42dee9464fed8c6b1d1e5bff0b7961f9
Andrew Che <andrew@codeninja55.me>
2017-10-02 03:45:57 +1100

    Added throwing a new ImportEmptyException, handled in DataStoreDAO
    Checks if there are no lines in the file.

--- com.cn55.marvel/src/application/model/file_connectors/CardsImport.java
+++ com.cn55.marvel/src/application/model/file_connectors/CardsImport.java
@@ -3,31 +3,38 @@ package application.model.file_connectors;
 import application.model.Generator;
 import application.model.card.*;
 import application.model.dao.DataStoreDAO;
+import application.model.exceptions.ImportEmptyException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 
 public class CardsImport implements ImportFromCSV {
-    public void executeImport(DataStoreDAO dataStore, BufferedReader reader) throws IOException {
-        reader.lines().forEach(line -> {
-            String[] readLine = line.split(CSV.DEFAULT_SEPARATOR_STR);
-            // [1] = ID | [2] = cardType | [3] = name | [4] = email | [5] = balance | [6] = points
-            Card importCard = null;
-            String cardID = readLine[1];
+    public void executeImport(DataStoreDAO dataStore, BufferedReader reader) throws IOException, ImportEmptyException {
+        if (reader.lines().count() == 0) {
+            reader.close();
+            throw new ImportEmptyException("CardsStorage.csv is empty");
+        } else {
+            reader.lines().forEach(line -> {
+                String[] readLine = line.split(CSV.DEFAULT_SEPARATOR_STR);
+                // [1] = ID | [2] = cardType | [3] = name | [4] = email | [5] = balance | [6] = points
+                Card importCard = null;
+                String cardID = readLine[1];
 
-            if (CardType.AnonCard.equalsName(readLine[2]))
-                importCard = new AnonCard(cardID, Double.parseDouble(readLine[5]));
-            else if (CardType.BasicCard.equalsName(readLine[2]))
-                importCard = new BasicCard(cardID, readLine[3], readLine[4], Double.parseDouble(readLine[5]),
-                        Double.parseDouble(readLine[6]));
-            else if (CardType.PremiumCard.equalsName(readLine[2]))
-                importCard = new PremiumCard(cardID, readLine[3], readLine[4], Double.parseDouble(readLine[5]),
-                        Double.parseDouble(readLine[6]));
+                if (CardType.AnonCard.equalsName(readLine[2]))
+                    importCard = new AnonCard(cardID, Double.parseDouble(readLine[5]));
+                else if (CardType.BasicCard.equalsName(readLine[2]))
+                    importCard = new BasicCard(cardID, readLine[3], readLine[4], Double.parseDouble(readLine[5]),
+                            Double.parseDouble(readLine[6]));
+                else if (CardType.PremiumCard.equalsName(readLine[2]))
+                    importCard = new PremiumCard(cardID, readLine[3], readLine[4], Double.parseDouble(readLine[5]),
+                            Double.parseDouble(readLine[6]));
 
-            Generator.updateCardIDCounter();
-            assert importCard != null;
-            dataStore.createCard(importCard);
-        });
-        reader.close();
+                Generator.updateCardIDCounter();
+                assert importCard != null;
+                dataStore.createCard(importCard);
+            });
+            reader.close();
+        }
+
     }
 }
--- com.cn55.marvel/src/application/model/file_connectors/CategoriesImport.java
+++ com.cn55.marvel/src/application/model/file_connectors/CategoriesImport.java
@@ -3,21 +3,27 @@ package application.model.file_connectors;
 import application.model.Generator;
 import application.model.category.Category;
 import application.model.dao.DataStoreDAO;
+import application.model.exceptions.ImportEmptyException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 
 public class CategoriesImport implements ImportFromCSV {
-    public void executeImport(DataStoreDAO dataStore, BufferedReader reader) throws IOException {
-        reader.lines().forEach(line -> {
-            String[] readLine = line.split(CSV.DEFAULT_SEPARATOR_STR);
-            // readLine[0] = id | [1] = name | [2] = description | [3] = amount
-            Category importedCategory = new Category(Integer.parseInt(readLine[1]), readLine[2],
-                    readLine[3], Double.parseDouble(readLine[4]));
+    public void executeImport(DataStoreDAO dataStore, BufferedReader reader) throws ImportEmptyException, IOException {
+        if (reader.lines().count() == 0) {
+            reader.close();
+            throw new ImportEmptyException("CategoriesStorage.csv is empty");
+        } else {
+            reader.lines().forEach(line -> {
+                String[] readLine = line.split(CSV.DEFAULT_SEPARATOR_STR);
+                // readLine[0] = id | [1] = name | [2] = description | [3] = amount
+                Category importedCategory = new Category(Integer.parseInt(readLine[1]), readLine[2],
+                        readLine[3], Double.parseDouble(readLine[4]));
 
-            Generator.updateCategoryIDCounter();
-            dataStore.createCategory(importedCategory);
-        });
-        reader.close();
+                Generator.updateCategoryIDCounter();
+                dataStore.createCategory(importedCategory);
+            });
+            reader.close();
+        }
     }
 }
